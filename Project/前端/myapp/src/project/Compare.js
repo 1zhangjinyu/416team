ed008a09df3bb42292acd06c179f53ad30bc743d
LIYAMANO-o <12277260741@qq.com>
2020-11-27 14:15:27 +0800

    11.27

--- Project/前端/myapp/src/project/Compare.js
+++ Project/前端/myapp/src/project/Compare.js
@@ -5,16 +5,21 @@ export default class Compare extends Component {
         return (
             <div  style={{backgroundColor:'rgb(247,240,240)'}}>
                  <div className='foodrank-header'>
-                    <span className="iconfont icon-jiantouarrowhead7" onClick={()=>this.props.history.push('/')}></span>
+                    <span className="iconfont icon-jiantouarrowhead7" onClick={()=>this.props.history.push('/home')}></span>
                     <span>对比详情</span>
                 </div>
                 <div style={{width:'400px',height:'150px',backgroundColor:'rgb(247,240,240)'}}>
-                <div id="comleft">
-                    <p style={{fontSize:'30px',paddingLeft:'35px',paddingTop:'25px',color:'red'}}>+</p>
+                <div id="comleft" onClick={()=>this.props.history.push('/home/search1')}>
+                    <p style={{fontSize:'30px',paddingLeft:'35px',color:'red'}}>+</p>
                 </div>
-                <div id="conright">
-
+                <img src={require('./image/vs.png')} style={{width:'50px',height:'50px',marginLeft:'-160px',marginTop:'50px'}}/>
+                <div id="conright" onClick={()=>this.props.history.push('/home/search1')}>
+                    <p style={{fontSize:'30px',paddingLeft:'35px',color:'red'}}>+</p>
                 </div>
+                </div>
+                <div style={{width:'100%',height:'600px',backgroundColor:'white'}}>
+                    <p style={{fontSize:'18px',textAlign:'center',paddingTop:'20px'}}>营养元素</p>
+                    <p style={{color:'#ccc'}}>_____________________________________________________________</p>
                 </div>
                
                
---
+++ Project/前端/myapp/src/project/Search1.js
@@ -0,0 +1,36 @@
+import { SearchBar, Button, WhiteSpace, WingBlank } from 'antd-mobile';
+import React from 'react'
+class Search1 extends React.Component {
+  state = {
+    value: '食物',
+  };
+ 
+  onChange= (value) => {
+    this.setState({ value });
+  };
+  clear = () => {
+    this.setState({ value: '' });
+  };
+  constructor(props) {
+    super(props);
+    
+  }
+  render() {
+    return (<div>
+      
+      <SearchBar
+        value={this.state.value}
+        placeholder="Search"
+        onSubmit={value => console.log(value, 'onSubmit')}
+        onClear={value => console.log(value, 'onClear')}
+        onFocus={() => console.log('onFocus')}
+        onBlur={() => console.log('onBlur')}
+        onCancel={()=>this.props.history.push('/home/com')}
+        showCancelButton
+        onChange={this.onChange}
+      />
+    </div>);
+  }
+}
+
+export default Search1;
